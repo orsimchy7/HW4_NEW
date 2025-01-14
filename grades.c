eee704aec396b006a0c4a4bc3d418d00567ce101
Mamat <orsimchy100@gmail.com>
2025-01-14 13:08:42 +0200

    or1401

--- grades.c
+++ grades.c
@@ -9,16 +9,45 @@
 
 typedef struct grades {
   struct list* students_list,
-  student_clone //functions needed?
 }
 
 //to implement:
   //1. init
   //2. destroy
 
-int grades_add_student(struct grades *grades, const char *name, int id) {
-  if (!grades) return; //what to return exactly? (same problem as in cource.c ...)
-  //look for id in grades & if exist return failure;
-  struct student newStudent = student_init(name, id);
-  return val = list_push_back(grades->students_list, newStudent);
+struct grades* grades_init(){
+	grades->students_list = list_init(student_clone , student_destroy);
+	//check if this is the right way to transfer pointers to funcs
+}
+
+
+
+
+int grades_add_student(grades* grades, const char *name, int id) {
+  if (!grades){
+  	return 1;
+  } 
+  
+  //check if id is new in the list
+  struct it* = list_begin(grades->students_list);
+
+	while(it==!NULL){
+		//there are courses in list to print
+		student* s = (student*)list_get(it);
+		tmp_id = s->ID;
+
+		if(tmp_id == id){
+			return 1;
+			//id is not uniqe- fail
+		}
+
+		it = list_next(it);
+	}
+	//
+
+  	student* newStudent = student_init(name, id);
+  	int val = list_push_back(grades->students_list, newStudent);
+  	free(newStudent);
+
+  	return val;
 }
