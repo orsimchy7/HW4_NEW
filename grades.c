c03019456dc110ce841fc4c9473612933e9b06c2
OrSimchy <102531228+orsimchy7@users.noreply.github.com>
2025-01-26 17:31:51 +0200

    Update grades.c

--- grades.c
+++ grades.c
@@ -382,6 +382,7 @@ int grades_add_grade(struct grades *grades,
 //returns -1 on error (and when fails? check)
 float grades_calc_avg(struct grades *grades, int id, char **out){
 	if (!grades) {
+		*out = NULL;
 		return -1;
 	}
 	float avg = 0;
